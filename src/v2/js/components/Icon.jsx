12ce41c7309d66239cd94c4dbe2ae14f2c8e4635
pacahon <pacabest@gmail.com>
2021-03-05 11:44:24 +0300

    fix: reflect SITE-147 changes into js code

--- src/v2/js/components/Icon.jsx
+++ src/v2/js/components/Icon.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 
 const Icon = ({ id, className = '' }) => (
     <svg aria-hidden="true"
-         className={cx(`sprite-img svg-icon _${id}`, className)}
+         className={cx(`sprite-img svg-icon _icon-${id}`, className)}
          xmlnsXlink="http://www.w3.org/1999/xlink">
-        <use xlinkHref={`#${id}`}/>
+        <use xlinkHref={`#icon-${id}`}/>
     </svg>
 );
 

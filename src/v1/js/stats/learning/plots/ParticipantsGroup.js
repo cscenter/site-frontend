b7369ee36e71ff9548a4ff902cef8549848a2def
pacahon <pacabest@gmail.com>
2021-03-19 19:20:30 +0300

    style: prettify some js sources

--- src/v1/js/stats/learning/plots/ParticipantsGroup.js
+++ src/v1/js/stats/learning/plots/ParticipantsGroup.js
@@ -1,75 +1,78 @@
 import { select as d3Select } from 'd3-selection';
-import c3 from "c3";
+import c3 from 'c3';
 import $ from 'jquery';
-import {StudentRoles, URLS} from 'stats/utils';
+import { StudentRoles, URLS } from 'stats/utils';
 import i18n from 'stats/i18n';
 
 class ParticipantsGroup {
-    static ENDPOINT_URI = "stats-api:stats_learning_participants_group";
+  static ENDPOINT_URI = 'stats-api:stats_learning_participants_group';
 
-    constructor(id, options) {
-        this.id = `#${id}`;
-        this.student_types = StudentRoles;
-        this.state = {
-            data: {
-                type: 'pie',
-                columns: [],
-                order: null, // https://github.com/c3js/c3/issues/1945
-            }
-        };
-
-        this.plot = c3.generate({
-            bindto: this.id,
-            tooltip: {
-                format: {
-                    value: (value, ratio, id) => {
-                        return value + i18n.people_suffix;
-                    }
-                }
-            },
-            data: this.state.data
-        });
-
-        let promise = options.apiRequest ||
-                      this.getStats(options.course_session_id);
-        promise
-            .then(this.convertData)
-            .then(this.render)
-            .done(this.appendParticipantsInfo);
-    }
-
-    getStats(course_session_id) {
-        let dataURL = URLS[this.constructor.ENDPOINT_URI](course_session_id);
-        return $.getJSON(dataURL);
-    }
-
-    convertData = (rawJSON) => {
-        let columns = [];
-        rawJSON.forEach((e) => {
-            columns.push([this.student_types[e.type], e.count]);
-        });
-        this.state.data.columns = columns;
-        return rawJSON;
+  constructor(id, options) {
+    this.id = `#${id}`;
+    this.state = {
+      data: {
+        type: 'pie',
+        columns: [],
+        order: null // https://github.com/c3js/c3/issues/1945
+      }
     };
 
-    render = (columns) => {
-        this.plot.load(this.state.data);
-        return columns;
-    };
+    this.plot = c3.generate({
+      bindto: this.id,
+      tooltip: {
+        format: {
+          value: (value, ratio, id) => {
+            return value + i18n.people_suffix;
+          }
+        }
+      },
+      data: this.state.data
+    });
 
-    appendParticipantsInfo = (rawJSON) => {
-        let total = 0;
-        rawJSON.forEach((e) => {
-            total += e.count;
-        });
-        d3Select(this.id).insert('div', ":first-child")
-            .attr('class', 'info')
-            .selectAll('div')
-            .data([total])
-            .enter()
-            .append('div')
-            .text(d => i18n.total_participants + ': ' + d);
-    };
+    let promise =
+      options.apiRequest || this.getStats(options.course_session_id);
+    promise
+      .then(this.convertData)
+      .then(this.render)
+      .done(this.appendParticipantsInfo);
+  }
+
+  getStats(course_session_id) {
+    let dataURL = URLS[this.constructor.ENDPOINT_URI](course_session_id);
+    return $.getJSON(dataURL);
+  }
+
+  convertData = rawJSON => {
+    let columns = [];
+    rawJSON.forEach(e => {
+      const label = Object.prototype.hasOwnProperty.call(StudentRoles, e.type)
+        ? StudentRoles[e.type]
+        : 'Unknown Type';
+      columns.push([label, e.count]);
+    });
+    this.state.data.columns = columns;
+    return rawJSON;
+  };
+
+  render = columns => {
+    this.plot.load(this.state.data);
+    return columns;
+  };
+
+  appendParticipantsInfo = rawJSON => {
+    let total = 0;
+    rawJSON.forEach(e => {
+      total += e.count;
+    });
+    d3Select(this.id)
+      .insert('div', ':first-child')
+      .attr('class', 'info')
+      .selectAll('div')
+      .data([total])
+      .enter()
+      .append('div')
+      .text(d => i18n.total_participants + ': ' + d);
+  };
 }
 
 export default ParticipantsGroup;
--- src/v1/js/stats/utils.js
+++ src/v1/js/stats/utils.js
@@ -1,28 +1,29 @@
-import i18n from "./i18n";
+import i18n from './i18n';
 
 import template from 'lodash-es/template';
 
 export const GROUPS = {
-    1: i18n.groups.STUDENT_CENTER,
-    4: i18n.groups.VOLUNTEER,
-    3: i18n.groups.GRADUATE_CENTER
+  1: i18n.groups.STUDENT_CENTER,
+  4: i18n.groups.VOLUNTEER,
+  3: i18n.groups.GRADUATE_CENTER
 };
 
 export const StudentRoles = {
-    regular: i18n.studentTypes.REGULAR,
-    invited: i18n.studentTypes.INVITED,
+  regular: i18n.studentTypes.REGULAR,
+  invited: i18n.studentTypes.INVITED,
+  graduate: i18n.studentTypes.GRADUATE
 };
 
 export const COLOR_PALETTE = [
-    '#5cb85c',
-    '#f96868',
-    '#F6BE80',
-    '#515492',
-    '#4F86A0'
+  '#5cb85c',
+  '#f96868',
+  '#F6BE80',
+  '#515492',
+  '#4F86A0'
 ];
 
 export const URLS = window.URLS;
 
-export function getTemplate (id) {
-    return template(document.getElementById(id).innerHTML);
+export function getTemplate(id) {
+  return template(document.getElementById(id).innerHTML);
 }

be5c3392faa18b2f8e3d69eeea805a9988fd9b03
pacahon <pacabest@gmail.com>
2021-04-14 18:22:05 +0300

    refactor: set timepicker step to 1 min

--- src/v1/js/teaching/index.js
+++ src/v1/js/teaching/index.js
@@ -1,73 +1,72 @@
-import {showComponentError, getSections} from 'utils';
+import { showComponentError, getSections } from 'utils';
 
 $(document).ready(function () {
-    let sections = getSections();
-    if (sections.includes("tooltips")) {
-        let defaultWhiteList = $.fn.tooltip.Constructor.DEFAULTS.whiteList;
-        defaultWhiteList.dl = ['class'];
-        defaultWhiteList.dd = [];
-        defaultWhiteList.dt = [];
-        $('[data-toggle="tooltip"]').tooltip();
-    }
-    if (sections.includes("datetimepickers")) {
-        import('components/forms')
-            .then(m => {
-                m.initDatePickers();
-                m.initTimePickers();
-            })
-            .catch(error => showComponentError(error));
-    }
-    if (sections.includes("selectpickers")) {
-        import('components/forms')
-            .then(m => {
-                m.initSelectPickers();
-            })
-            .catch(error => showComponentError(error));
-    }
-    if (sections.includes("studentAssignment")) {
-        import(/* webpackChunkName: "gradebook" */ 'teaching/studentAssignment')
-            .then(module => {
-                const component = module.default;
-                component.launch();
-            })
-            .catch(error => showComponentError(error));
-    }
+  let sections = getSections();
+  if (sections.includes('tooltips')) {
+    let defaultWhiteList = $.fn.tooltip.Constructor.DEFAULTS.whiteList;
+    defaultWhiteList.dl = ['class'];
+    defaultWhiteList.dd = [];
+    defaultWhiteList.dt = [];
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+  if (sections.includes('datetimepickers')) {
+    import('components/forms')
+      .then(m => {
+        m.initDatePickers();
+        m.initTimePickers();
+      })
+      .catch(error => showComponentError(error));
+  }
+  if (sections.includes('selectpickers')) {
+    import('components/forms')
+      .then(m => {
+        m.initSelectPickers();
+      })
+      .catch(error => showComponentError(error));
+  }
+  if (sections.includes('studentAssignment')) {
+    import(/* webpackChunkName: "gradebook" */ 'teaching/studentAssignment')
+      .then(module => {
+        const component = module.default;
+        component.launch();
+      })
+      .catch(error => showComponentError(error));
+  }
 
-    if (sections.includes("gradebook")) {
-        import(/* webpackChunkName: "gradebook" */ 'teaching/gradebook')
-            .then(module => {
-                const component = module.default;
-                component.launch();
-            })
-            .catch(error => showComponentError(error));
-    } else if (sections.includes("submissions")) {
-        import(/* webpackChunkName: "submissions" */ 'teaching/submissions')
-            .then(m => {
-                const component = m.default;
-                component.launch();
-            })
-            .catch(error => showComponentError(error));
-    } else if (sections.includes("assignmentForm")) {
-        $('.has-popover').popover({
-            container: 'body',
-            html: true,
-            placement: 'auto',
-            trigger: 'hover',
-            content: function () {
-                let helpBlockId = $(this).data('target');
-                return $(helpBlockId).html();
-            }
-        });
+  if (sections.includes('gradebook')) {
+    import(/* webpackChunkName: "gradebook" */ 'teaching/gradebook')
+      .then(module => {
+        const component = module.default;
+        component.launch();
+      })
+      .catch(error => showComponentError(error));
+  } else if (sections.includes('submissions')) {
+    import(/* webpackChunkName: "submissions" */ 'teaching/submissions')
+      .then(m => {
+        const component = m.default;
+        component.launch();
+      })
+      .catch(error => showComponentError(error));
+  } else if (sections.includes('assignmentForm')) {
+    $('.has-popover').popover({
+      container: 'body',
+      html: true,
+      placement: 'auto',
+      trigger: 'hover',
+      content: function () {
+        let helpBlockId = $(this).data('target');
+        return $(helpBlockId).html();
+      }
+    });
 
-        const checkingSystemFieldSet = $('#checking-system-info');
-        const checkerSubmissionFormats = checkingSystemFieldSet.data('display');
-        $('select[name="submission_type"]').change(function(e) {
-            if (this.value.length > 0 &&
-                checkerSubmissionFormats.includes(this.value)) {
-                checkingSystemFieldSet.removeClass('hidden');
-            } else {
-                checkingSystemFieldSet.addClass('hidden');
-            }
-        });
-    }
+    const checkingSystemFieldSet = $('#checking-system-info');
+    const checkerSubmissionFormats = checkingSystemFieldSet.data('display');
+    $('select[name="submission_type"]').change(function (e) {
+      if (this.value.length > 0 && checkerSubmissionFormats.includes(this.value)) {
+        checkingSystemFieldSet.removeClass('hidden');
+      } else {
+        checkingSystemFieldSet.addClass('hidden');
+      }
+    });
+  }
 });
--- src/v1/js/utils.js
+++ src/v1/js/utils.js
@@ -1,39 +1,38 @@
-import {get as getCookie} from 'es-cookie';
+import { get as getCookie } from 'es-cookie';
 import template from 'lodash-es/template';
 
 export function getLocalStorageKey(textarea) {
-    return (window.location.pathname.replace(/\//g, "_")
-    + "_" + textarea.name);
+  return window.location.pathname.replace(/\//g, '_') + '_' + textarea.name;
 }
 
 export function csrfSafeMethod(method) {
-    // These HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  // These HTTP methods do not require CSRF protection
+  return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
 }
 
 export function getCSRFToken() {
-    return getCookie(window.__CSC__.config.csrfCookieName);
+  return getCookie(window.__CSC__.config.csrfCookieName);
 }
 
-export function getTemplate (id) {
-    return template(document.getElementById(id).innerHTML);
+export function getTemplate(id) {
+  return template(document.getElementById(id).innerHTML);
 }
 
-export function createNotification(msg, theme='default', position='bottom-right') {
-    $.jGrowl(msg, { theme: theme, position: position });
+export function createNotification(msg, theme = 'default', position = 'bottom-right') {
+  $.jGrowl(msg, { theme: theme, position: position });
 }
 
-export function showComponentError(error, msg='An error occurred while loading the component') {
-    console.error(error);
-    createNotification(msg, 'error');
+export function showComponentError(error, msg = 'An error occurred while loading the component') {
+  console.error(error);
+  createNotification(msg, 'error');
 }
 
 // TODO: share with v2?
 export function getSections() {
-    if (document.body.hasAttribute("data-init-sections")) {
-        let sections = document.body.getAttribute("data-init-sections");
-        return sections.split(",");
-    } else {
-        return [];
-    }
+  if (document.body.hasAttribute('data-init-sections')) {
+    let sections = document.body.getAttribute('data-init-sections');
+    return sections.split(',');
+  } else {
+    return [];
+  }
 }

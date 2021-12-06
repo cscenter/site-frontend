d7342f2eae8b56380fc34877a0c391b3807259cb
pacahon <pacabest@gmail.com>
2021-12-06 20:39:32 +0300

    fix: avoid collisions with b3 panel-groups

--- src/v1/js/center/index.js
+++ src/v1/js/center/index.js
@@ -1,17 +1,14 @@
-import { showComponentError } from 'utils';
 import { reviewFormValidation } from './projects_report';
 
 // Let's leave it here for now
 $(function () {
-  if (document.getElementsByClassName('panel-group').length > 0) {
-    $('.panel-group').on('click', '.panel-heading', function (e) {
-      // Replace js animation with css.
-      e.preventDefault();
-      const open = $(this).attr('aria-expanded') === 'true';
-      $(this).next().toggleClass('collapse').attr('aria-expanded', !open);
-      $(this).attr('aria-expanded', !open);
-    });
-  }
+  $('.panel-group').on('click', '.panel-heading._arrowed', function (e) {
+    // Replace js animation with css.
+    e.preventDefault();
+    const open = $(this).attr('aria-expanded') === 'true';
+    $(this).next().toggleClass('collapse').attr('aria-expanded', !open);
+    $(this).attr('aria-expanded', !open);
+  });
 
   // Leave it here for now
   reviewFormValidation();

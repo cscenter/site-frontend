7fc89861fd66baf902c87ae19177692a56e79e63
Sokov Ilya <94356906+sokovis@users.noreply.github.com>
2022-12-22 14:54:01 +0300

    feat(LMS-314): js for assignment balanced mode (#471)
    * feat: add js to new assignment appointment balanced mode
    
    * build: update v1 js bundle
    
    * build: delete old files

--- src/v1/js/teaching/assignmentForm.js
+++ src/v1/js/teaching/assignmentForm.js
@@ -57,3 +57,62 @@ function updateAssigneeModeAdditionSettings(mode) {
     }
   }
 }
+
+let addButton = document.querySelector("#add-form");
+let bucketsContainer = document.querySelector("#buckets-formset")
+let emptyForm = document.querySelector("#buckets-empty-form").firstElementChild
+let totalFormsInput = document.querySelector("#id_bucket-TOTAL_FORMS");
+
+addButton.addEventListener('click', addBucket);
+for (let formIndex=0; formIndex < totalFormsInput.value; formIndex++) {
+    connectClearFillButtons(`bucket-${formIndex}`)
+}
+
+function connectClearFillButtons(formPrefix) {
+  let fillSG = document.querySelector(`#id_${formPrefix}-FILL-SG`)
+  let clearSG = document.querySelector(`#id_${formPrefix}-CLEAR-SG`)
+  let fillCT = document.querySelector(`#id_${formPrefix}-FILL-CT`)
+  let clearCT = document.querySelector(`#id_${formPrefix}-CLEAR-CT`)
+  fillSG.addEventListener('click', (e) => {
+    let sg_select = document.querySelector(`#id_${formPrefix}-student_groups`)
+    for (let opt of sg_select.options) {
+      opt.selected = 'selected';
+    }
+    sg_select.dispatchEvent(new Event('change'));
+  })
+  clearSG.addEventListener('click', (e) => {
+    let sg_select = document.querySelector(`#id_${formPrefix}-student_groups`)
+    for (let opt of sg_select.options) {
+      opt.selected = '';
+    }
+    sg_select.dispatchEvent(new Event('change'));
+  })
+  fillCT.addEventListener('click', (e) => {
+    let ct_select = document.querySelector(`#id_${formPrefix}-teachers`)
+    for (let opt of ct_select.options) {
+      opt.selected = 'selected';
+    }
+    ct_select.dispatchEvent(new Event('change'));
+  })
+  clearCT.addEventListener('click', (e) => {
+    let ct_select = document.querySelector(`#id_${formPrefix}-teachers`)
+    for (let opt of ct_select.options) {
+      opt.selected = '';
+    }
+    ct_select.dispatchEvent(new Event('change'));
+  })
+
+}
+
+function addBucket(event) {
+  event.preventDefault();
+
+  let totalFormsNum = totalFormsInput.value - 1;
+  let newForm = emptyForm.cloneNode(true);
+  let formRegex = RegExp(`__prefix__`,'g');
+  totalFormsNum++;
+  newForm.innerHTML = newForm.innerHTML.replace(formRegex, totalFormsNum);
+  bucketsContainer.appendChild(newForm);
+  totalFormsInput.setAttribute('value', totalFormsNum + 1);
+  connectClearFillButtons(`bucket-${totalFormsNum}`)
+}

e6db744cbe7843167f65cef7e64c01ae55eec15e
Дмитрий Че <39742182+Dmi4er4@users.noreply.github.com>
2025-04-15 18:07:07 +0300

    Hint on click (#509)
    * working but rough
    
    * done

--- src/v2/js/components/Tooltip.jsx
+++ src/v2/js/components/Tooltip.jsx
@@ -1,25 +1,45 @@
 import * as PropTypes from 'prop-types';
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { Tooltip as BaseTooltip } from 'react-tippy';
 
-export const Tooltip = ({ title, children, ...options }) => (
-  <BaseTooltip
-    animation={`fade`}
-    arrow={true}
-    arrowSize={`small`}
-    size={`regular`}
-    theme={`dark`}
-    {...options}
-    title={title}
-  >
-    {children}
-  </BaseTooltip>
-);
+export const Tooltip = ({ title, children, interactive: interactiveProp, ...options }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(pointer: coarse)');
+    setIsMobile(mediaQuery.matches);
+    const handler = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener('change', handler);
+    return () => mediaQuery.removeEventListener('change', handler);
+  }, []);
+
+  const tippyProps = {
+    animation: 'fade',
+    arrow: true,
+    arrowSize: 'small',
+    size: 'regular',
+    theme: 'dark',
+    ...options,
+    title: title,
+    ...(isMobile
+      ? {
+          trigger: 'click',
+          interactive: true,
+          hideOnClick: false
+        }
+      : {
+          trigger: 'mouseenter focus',
+          interactive: interactiveProp || false
+        })
+  };
+
+  return <BaseTooltip {...tippyProps}>{children}</BaseTooltip>;
+};
 
 Tooltip.propTypes = {
   title: PropTypes.string,
-  children: PropTypes.element.isRequired
+  children: PropTypes.element.isRequired,
+  interactive: PropTypes.bool
 };
 
 export const Hint = ({ ...options }) => (
